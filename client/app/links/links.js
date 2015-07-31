94c93083f2c1b5686a92d8e47b827d89cfd79f0a
dierat <diedra.rater@gmail.com>
2015-07-31 16:58:10 -0700

    fills out links template and adds dummy objects to links controller as well as initial function definitions to be written later

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,34 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  angular.extend($scope, Links);
+})
+.factory('Links', function(){
+
+  var linksArray = [
+    {
+      title: "Hack Reactor",
+      url: "www.hackreactor.com",
+      'base-url': "http://localhost/",
+      code: "nfoqwhp9p4t8qp",
+      visits: 4
+    },
+    {
+      title: "Stack Overflow",
+      url: "www.stackoverflow.com",
+      'base-url': "http://localhost/",
+      code: "abcdefg123",
+      visits: 5
+    }
+  ];
+
+  var getLinks = function(){};
+
+  var goToLink = function(){};
+
+  return {
+    linksArray: linksArray,
+    getLinks: getLinks,
+    goToLink: goToLink
+  };
 });
