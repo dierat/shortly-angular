33056bca1c87bead4bb0d93b43bcf2a72f4e4d3a
dierat <diedra.rater@gmail.com>
2015-07-31 19:59:19 -0700

    adds post request to send links to the database

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,30 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $location, Links, Shorten) {
+  angular.extend($scope, Shorten);
+
+  $scope.shortenLink = function(){
+    // grab the input text from form
+    // send input to sever via post request
+    $scope.sendData($scope.newLink).catch(function(err){
+      console.error(err);
+    });
+  }
+})
+.factory('Shorten', function($http){
+  var sendData = function(newLink){
+    console.log("newLink = ", newLink);
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: {url: newLink}
+    })
+    .then(function(res){
+      console.log("sucess??! res = ", res);
+    });
+  };
+
+  return {
+    sendData: sendData
+  };
 });
--- server/links/linkController.js
+++ server/links/linkController.js
@@ -35,6 +35,7 @@ module.exports = {
   newLink: function (req, res, next) {
     var url = req.body.url;
     console.log(req.body);
+    console.log(req.data);
     if (!util.isValidUrl(url)) {
       return next(new Error('Not a valid url'));
     }
