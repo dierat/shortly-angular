c4af37af7dc6782e58c4e2dbdb321e6d33e49b86
dierat <diedra.rater@gmail.com>
2015-07-31 19:12:54 -0700

    adds function code for grabbing data from the database to fill the links template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,33 +2,50 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   angular.extend($scope, Links);
+  $scope.getLinks = function(){
+  // console.log('Get links button clicked');
+    Links.getData().then(function(data){
+      $scope.data = data;
+    }).catch(function(err){
+      console.error(err);
+    });
+  };
 })
-.factory('Links', function(){
+.factory('Links', function($http){
 
-  var linksArray = [
-    {
-      title: "Hack Reactor",
-      url: "www.hackreactor.com",
-      'base-url': "http://localhost/",
-      code: "nfoqwhp9p4t8qp",
-      visits: 4
-    },
-    {
-      title: "Stack Overflow",
-      url: "www.stackoverflow.com",
-      'base-url': "http://localhost/",
-      code: "abcdefg123",
-      visits: 5
-    }
-  ];
+  // var linksArray = [
+  //   {
+  //     title: "Hack Reactor",
+  //     url: "www.hackreactor.com",
+  //     'base-url': "http://localhost/",
+  //     code: "nfoqwhp9p4t8qp",
+  //     visits: 4
+  //   },
+  //   {
+  //     title: "Stack Overflow",
+  //     url: "www.stackoverflow.com",
+  //     'base-url': "http://localhost/",
+  //     code: "abcdefg123",
+  //     visits: 5
+  //   }
+  // ];
 
-  var getLinks = function(){};
+  var getData = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    }).then(function(res){
+      console.log('res is', res, 'res.data is', res.data);
+      return res.data;
+    });
+  };
 
-  var goToLink = function(){};
+  var goToLink = function(){
+    // console.log('A link was clicked');
+  };
 
   return {
-    linksArray: linksArray,
-    getLinks: getLinks,
-    goToLink: goToLink
+    goToLink: goToLink,
+    getData: getData
   };
 });
